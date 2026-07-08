5314cbc5d453a4848e4467bbecf81199636b6eda
Daniel Allen <dan@aeternagroup.net>
2026-07-08 23:38:36 +0100

    Update to schema version v4.80.0

---
+++ main.tf
@@ -0,0 +1,24 @@
+locals {
+  iothub_dpses = { for k1, v1 in var.iothub_dpses : k1 => { allocation_policy = v1.allocation_policy, data_residency_enabled = v1.data_residency_enabled, ip_filter_rule = v1.ip_filter_rule, linked_hub = v1.linked_hub, location = v1.location, name = v1.name, public_network_access_enabled = v1.public_network_access_enabled, resource_group_name = v1.resource_group_name, sku = v1.sku, tags = v1.tags } }
+
+  iothub_dps_shared_access_policies = merge([
+    for k1, v1 in var.iothub_dpses : {
+      for k2, v2 in coalesce(v1.iothub_dps_shared_access_policies, {}) :
+      "${k1}/${k2}" => merge(v2, {
+        iothub_dps_name = module.iothub_dpses.iothub_dpses["${k1}"].name
+      })
+    }
+  ]...)
+}
+
+module "iothub_dpses" {
+  source       = "git::https://github.com/AeternaModules/azurerm_iothub_dps.git?ref=v4.80.0"
+  iothub_dpses = local.iothub_dpses
+}
+
+module "iothub_dps_shared_access_policies" {
+  source                            = "git::https://github.com/AeternaModules/azurerm_iothub_dps_shared_access_policy.git?ref=v4.80.0"
+  iothub_dps_shared_access_policies = local.iothub_dps_shared_access_policies
+  depends_on                        = [module.iothub_dpses]
+}
+
---
+++ variables.tf
@@ -0,0 +1,70 @@
+variable "iothub_dpses" {
+  description = <<EOT
+Map of iothub_dpses, attributes below
+Required:
+    - location
+    - name
+    - resource_group_name
+    - sku (block)
+Optional:
+    - allocation_policy
+    - data_residency_enabled
+    - public_network_access_enabled
+    - tags
+    - ip_filter_rule (block)
+    - linked_hub (block)
+Nested iothub_dps_shared_access_policies (azurerm_iothub_dps_shared_access_policy):
+    Required:
+        - name
+        - resource_group_name
+    Optional:
+        - enrollment_read
+        - enrollment_write
+        - registration_read
+        - registration_write
+        - service_config
+EOT
+
+  type = map(object({
+    location                      = string
+    name                          = string
+    resource_group_name           = string
+    allocation_policy             = optional(string) # Default: "Hashed"
+    data_residency_enabled        = optional(bool)   # Default: false
+    public_network_access_enabled = optional(bool)   # Default: true
+    tags                          = optional(map(string))
+    sku = object({
+      capacity = number
+      name     = string
+    })
+    ip_filter_rule = optional(object({
+      action  = string
+      ip_mask = string
+      name    = string
+      target  = optional(string)
+    }))
+    linked_hub = optional(object({
+      allocation_weight       = optional(number) # Default: 1
+      apply_allocation_policy = optional(bool)   # Default: true
+      connection_string       = string
+      location                = string
+    }))
+    iothub_dps_shared_access_policies = optional(map(object({
+      name                = string
+      resource_group_name = string
+      enrollment_read     = optional(bool) # Default: false
+      enrollment_write    = optional(bool) # Default: false
+      registration_read   = optional(bool) # Default: false
+      registration_write  = optional(bool) # Default: false
+      service_config      = optional(bool) # Default: false
+    })))
+  }))
+
+  validation {
+    condition = alltrue(concat(
+      [for kk in keys(var.iothub_dpses) : !strcontains(kk, "/")],
+      flatten([for k0, v0 in var.iothub_dpses : [for kk in keys(coalesce(v0.iothub_dps_shared_access_policies, {})) : !strcontains(kk, "/")]])
+    ))
+    error_message = "Map keys in this package must not contain '/': it is used internally as a nesting-key separator, so a key containing it can silently collide two different nested entries into one. Rename the offending key(s)."
+  }
+}
